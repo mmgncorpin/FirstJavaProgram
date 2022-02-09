2d9bc47abb0020a20c7991b0bbe70e79d75350f9
Marie Madeleine Corpin <87760039+mmgncorpin@users.noreply.github.com>
2022-02-09 01:34:10 -0800

    Initial Commit

---
+++ Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("My name is Elaine Corpin");
+        System.out.println("I am 1000 years old");
+        System.out.println("My hometown in Paranaque City, Philippines");
+    }
+}
